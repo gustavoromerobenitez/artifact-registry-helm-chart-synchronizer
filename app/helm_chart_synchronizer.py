4ab30e766a99bb0f4ed2098a2b5f16de2c664707
Gustavo Romero Benitez <60758913+gustavoromerobenitez@users.noreply.github.com>
2024-03-23 12:10:55 +0100

    Proxy and authentication updates

--- app/helm_chart_synchronizer.py
+++ app/helm_chart_synchronizer.py
@@ -6,6 +6,8 @@ import sys
 import yaml
 import argparse
 import inspect
+import time
+import re
 
 from functools import partial
 
@@ -83,7 +85,7 @@ def execute_cli_command (command, error_message, logs, debug=False, capture_outp
 #
 # This function is meant to be run as a Process returning the lists: logs and synced_charts
 #
-def sync_chart ( chart, verify_certificates, debug):
+def sync_chart ( chart, authenticated_registries, pause_between_operations, destination_registry, verify_certificates, debug):
 
   logs = []
   synced_charts = []
@@ -92,11 +94,22 @@ def sync_chart ( chart, verify_certificates, debug):
 
   source_registry = chart["registry"]
   source_repository = chart["repository"]
-  chart_name = chart["chart"]
+  source_chart_name = chart["chart"]
+
+  #
+  # If the destination regsitry requires authentication, retrieve its credentials
+  #  which should be present as environment variables
+  # 
+  username, password = None
+  if authenticated_registries is not None \
+        and authenticated_registries != [] \
+        and destination_registry in authenticated_registries:
+
+    (username, password) = get_credentials_for_registry(destination_registry)
+  
 
   destination_repo = chart["destination_repository"]
-  destination_registry = f"oci://{os.environ['ARTIFACT_REGISTRY_HOSTNAME']}/{os.environ['ARTIFACT_REGISTRY_PROJECT_ID']}"
-
+  
   # add the source repository to the path to keep the charts organised in tArtifact Registry
   destination_full_path = f"{destination_registry}/{destination_repo}/{source_repository}"
 
@@ -104,16 +117,24 @@ def sync_chart ( chart, verify_certificates, debug):
 
   if versions == []:
 
-      logs.append(f"[ERROR] At least one tag must be specified for {source_repository}/{chart_name} at {source_registry}")
+      logs.append(f"[ERROR] At least one tag must be specified for {source_repository}/{source_chart_name} at {source_registry}")
       errors += 1
 
   else:
 
+    command_optional_args = ""
+    if verify_certificates:
+      command_optional_args = f"{command_optional_args} --ca-file {CERTIFICATE_BUNDLE_LOCATION}"
+
+    if username is not None and password is not None:
+      command_optional_args = f"{command_optional_args} --username '{username}' --password '{password}'"
+
     try:
 
+      logs.append("[INFO] ---------------------------------------------------------------------------------------")
       logs.append(f"[INFO] Adding Helm repository locally: {source_registry}")
 
-      command = f"helm repo add --force-update {source_repository} https://{source_registry}"
+      command = f"helm repo add --force-update {command_optional_args} {source_repository} https://{source_registry}"
       error_message = f"Failed to add Helm repo locally: {source_registry}"
       execute_cli_command (command, error_message, logs, debug = debug )
 
@@ -125,6 +146,9 @@ def sync_chart ( chart, verify_certificates, debug):
       logs.append(f"[ERROR] Exception: {e} ")
       errors += len(versions)
 
+    finally:
+      time.sleep(pause_between_operations)
+
 
     if errors == 0:
 
@@ -134,36 +158,52 @@ def sync_chart ( chart, verify_certificates, debug):
 
         for version in versions:
 
-          logs.append("[INFO] ---------------------------------------------------------------------------------------")
-          logs.append(f"[INFO] PROCESSING {source_repository}/{chart_name} version {version}")
+          logs.append(f"[INFO] PROCESSING {source_repository}/{source_chart_name} version {version}")
+
+          destination_chart_name = source_chart_name
 
           try:
 
-            logs.append(f"[INFO] Pulling Helm chart {source_repository}/{chart_name} version {version}")
+            logs.append(f"[INFO] Pulling Helm chart {source_repository}/{source_chart_name} version {version}")
 
-            command = f"helm pull {source_repository}/{chart_name} --version {version}"
-
-            if verify_certificates:
-              command = f"{command} --ca-file {CERTIFICATE_BUNDLE_LOCATION}"
-
-            error_message = f"Failed to pull chart {source_repository}/{chart_name} from {source_registry}"
+            command = f"helm pull {source_repository}/{source_chart_name} --version {version} {command_optional_args}"
+            error_message = f"Failed to pull chart {source_repository}/{source_chart_name} from {source_registry}"
             execute_cli_command (command, error_message, logs, debug = debug )
 
-            # Generate the pulled chart file name
-            pulled_chart = f"{chart_name}-{version}.tgz"
+            pulled_chart_file_name = f"{source_chart_name}-{version}.tgz"
 
-            logs.append(f"[INFO] Pushing Helm chart file {pulled_chart} to {destination_full_path}")
+            #
+            # If the Helm Chart name contains upper case letters, it will be rejected by Artifact Registry
+            #   We need to edit the chart name in the Chart.yaml included in the tgz file.
+            #
+            if re.search(r'^.+[A-Z]', pulled_chart_file_name) is not None:
+              
+              new_pulled_chart_file_name = re.sub(r'([A-Z])', lambda m: f"-{m.group(0).lower()}", pulled_chart_file_name)
+              new_chart_name = re.sub(r'([A-Z])', lambda m: f"-{m.group(0).lower()}", source_chart_name)
 
-            command = f"helm push {pulled_chart} {destination_full_path}"
-            error_message = f"Failed to push chart file {pulled_chart} to {destination_full_path}"
+              command = f"tar xf {pulled_chart_file_name} && cd {source_chart_name} && sed -i 's|{source_chart_name}|{new_chart_name}|g' Chart.yaml && helm package . && mv {new_pulled_chart_file_name} ../ && cd .. && rm -rf {source_chart_name}/"
+              error_message = f"Failed to replace the chart name {source_chart_name} with {new_chart_name} in Chart.yaml inside the chart file {pulled_chart_file_name}"
+              execute_cli_command (command, error_message, logs, debug = debug )
+
+              pulled_chart_file_name = new_pulled_chart_file_name
+              destination_chart_name = new_chart_name
+
+
+            logs.append(f"[INFO] Pushing Helm chart file {pulled_chart_file_name} to {destination_full_path}")
+
+            command = f"helm push {pulled_chart_file_name} {destination_full_path}"
+            error_message = f"Failed to push chart file {pulled_chart_file_name} to {destination_full_path}"
             execute_cli_command (command, error_message, logs, debug = debug )
 
             logs.append(f"[INFO] SUCCESS - chart {chart_name} version {version} pushed to {destination_full_path} ")
-            synced_charts.append(f"{pulled_chart}")
+            synced_charts.append(f"{pulled_chart_file_name}")
 
           except Exception as e:
             logs.append(f"[ERROR] Exception: {e} ")
             errors +=1
+          
+          finally:
+            time.sleep(pause_between_operations)
 
 
   result["logs"] = logs
@@ -171,6 +211,29 @@ def sync_chart ( chart, verify_certificates, debug):
   result["errors"] = errors
 
   return result
+
+
+
+
+########################################################################
+#
+# Given a Helm chart registry, it retrieves it credentials 
+#   from environment variables, if they are defined
+#
+def get_credentials_for_registry(registry):
+
+  # The username and password should be present in environment variables
+  #  named like the registry they belong to,
+  #  i.e.: REGISTRY_1_MYORG_COM_USERNAME, REGISTRY_1_MY_ORG_COM_PASSWORD
+  username_env_var_name = f'{registry.upper().replace("-","_").replace(".","_")}_USERNAME'
+  password_env_var_name = f'{registry.upper().replace("-","_").replace(".","_")}_PASSWORD'
+
+  check_environment_variables( [ username_env_var_name, password_env_var_name ], debug) or sys.exit(3)
+
+  username = os.environ[ username_env_var_name ]
+  password = os.environ[ password_env_var_name ]
+
+  return (username, password)
 
 
 ##########################################################
@@ -203,16 +266,7 @@ def authenticate_against_registries (config, artifact_registry_hostname, verify_
 
     for registry in config["authenticatedRegistries"]:
 
-      # The username and password should be present in environment variables
-      #  named like the registry they belong to,
-      #  i.e.: REGISTRY_1_MYORG_COM_USERNAME, REGISTRY_1_MY_ORG_COM_PASSWORD
-      username_env_var_name = f'{registry.upper().replace("-","_").replace(".","_")}_USERNAME'
-      password_env_var_name = f'{registry.upper().replace("-","_").replace(".","_")}_PASSWORD'
-
-      check_environment_variables( [ username_env_var_name, password_env_var_name ], debug) or sys.exit(3)
-
-      username = os.environ[ username_env_var_name ]
-      password = os.environ[ password_env_var_name ]
+      (username, password) = get_credentials_for_registry(registry)
 
       print(f"[INFO] Authenticating against the Helm registry at {registry} ...")
 
@@ -240,7 +294,7 @@ def authenticate_against_registries (config, artifact_registry_hostname, verify_
 #
 # Main
 #
-def main (charts_file, num_parallel_tasks):
+def main (charts_file, num_parallel_tasks, pause_between_operations):
 
   check_environment_variables( REQUIRED_ENVIRONMENT_VARIABLES[ "COMMON" ] ) or sys.exit(1)
 
@@ -253,6 +307,8 @@ def main (charts_file, num_parallel_tasks):
   print(f"[INFO] ARTIFACT_REGISTRY_PROJECT_ID: {artifact_registry_project_id}")
   print(f"[INFO] ARTIFACT_REGISTRY_HOSTNAME: {artifact_registry_hostname}")
   print(f"[INFO] VERIFY_CERTIFICATES: {verify_certificates}")
+  print(f"[INFO] PAUSE BETWEEN OPERATIONS: {pause_between_operations}")
+  print(f"[INFO] NUM PARALLEL TASKS: {num_parallel_tasks}")
   print(f"[INFO] DEBUG: {debug}")
   print("[INFO] ==================================================================================")
 
@@ -262,11 +318,12 @@ def main (charts_file, num_parallel_tasks):
     charts_file = f"{CURRENTDIR}/{charts_file}"
 
   config = None
-
   with open(charts_file) as f:
     config = yaml.safe_load(f)
 
   authenticate_against_registries (config, artifact_registry_hostname, verify_certificates = verify_certificates, debug = debug)
+
+  destination_registry = f"oci://{os.environ['ARTIFACT_REGISTRY_HOSTNAME']}/{os.environ['ARTIFACT_REGISTRY_PROJECT_ID']}"
 
   # Limit the number of parallel processes to avoid issues
   # when the chart list is shorter than the requested number of parallel processes
@@ -281,7 +338,14 @@ def main (charts_file, num_parallel_tasks):
   errors = 0
   chunk_size = int(len(charts)/num_parallel_tasks)
 
-  parallel_function = partial ( sync_chart, verify_certificates = verify_certificates, debug = debug )
+  authenticated_registries = None
+  if "authenticatedRegistries" in config.keys():
+    authenticated_registries = config['authenticatedRegistries']
+
+  parallel_function = partial ( sync_chart, authenticated_registries = authenticated_registries,
+                                            pause_between_operations = pause_between_operations,
+                                            destination_registry = destination_registry, 
+                                            verify_certificates = verify_certificates, debug = debug )
 
   with Pool(num_parallel_tasks) as p:
 
@@ -313,5 +377,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser( description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('-f','--charts-file', type = str,  required = False, default = "config/charts.yaml", metavar="< relative or absolute path to the charts YAML file >", help='The relative or absolute path to the YAML file that contains the list of Helm charts and versions to be synchronized.')
     parser.add_argument('-n','--num-parallel-tasks', type = int, required = False, default = 10, metavar="NUM_PARALLEL_TASKS", help='The max number of parallel tasks')
+    parser.add_argument('-p','--pause-between-operations', type = int, required = False, default = 10, metavar="PAUSE_BETWEEN_OPERATIONS", help='The pause in seconds between fetching and pulling each chart version')
     args = parser.parse_args()
-    main(args.charts_file, args.num_parallel_tasks)
+    main(args.charts_file, args.num_parallel_tasks, args.pause_between_operations)
